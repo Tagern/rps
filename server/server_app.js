2373caa651a784ac08b4f541cd3dae8b8aa3f4d3
Tom Thorley <tomthorley87@gmail.com>
2016-03-25 17:40:32 +0000

    hjgfew

--- server/server_app.js
+++ server/server_app.js
@@ -8,21 +8,24 @@
  * Module dependencies.
  */
 var http = require('http');
+var express = require('express');
+var app = express();
 var debug = require('debug')('rps:server');
+app.use(express.static(__dirname + '/../browser/'));
 
 // Define a port we want to listen to
 const PORT = process.env.PORT || 8080; //changed from 8080
 
 // We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
 
 /**
  * Create HTTP server.
  */
-var server = http.createServer(handleRequest);
+var server = http.createServer(app);
 
+app.get('/', function(req,res) {
+    res.sendFile("/browser/index.html", {"root":__dirname+"../browser/"});
+});
 /**
  * Listen on provided port, on all network interfaces.
  */
